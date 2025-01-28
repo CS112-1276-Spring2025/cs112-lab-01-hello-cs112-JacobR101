cc209e30ca432082c43bdce0f09d25b60ed6d46d
JacobR101 <kingkoconino@icloud.com>
2025-01-28 02:04:25 +0000

    Part 2 complete

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
+    
     public static void main(String[] args) {
         System.out.println("Hello world! from Jacob");
+        Printinfo();
+        System.out.println("do I like video games?: "+Likes("video games"));
     }
-}
+
+    public static void Printinfo() {
+        System.out.println("I enjoy playing video games, scrolling on instagram, and going out with my friends");
+    }
+    public static boolean Likes(String check){
+     String Mylikes =   "I enjoy playing video games, scrolling on instagram, and going out with my friends";
+     return(Mylikes.contains(check));
+
+    }
+    }
